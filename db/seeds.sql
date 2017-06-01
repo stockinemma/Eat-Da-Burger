1e402b4edf7d327102be04e42dc40295b22e9c54
stockinemma <stockinemma@gmail.com>
2017-06-01 16:59:24 -0400

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('California Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Black Bean Burger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Portabello Mushroom Burger');
+INSERT INTO burgers(burger_name) VALUES ('Italian Veggie Burger);
